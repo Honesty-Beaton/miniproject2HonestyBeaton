80be14425b70fb5aa29f8d8ad5fe171bc2865d82
Honesty Beaton <h_beaton@mail.fhsu.edu>
2024-09-23 21:03:31 -0500

    Added ability to grab specific currency rate from API request and store as a pandas DataFrame

--- main.py
+++ main.py
@@ -6,28 +6,24 @@
  exchange rate for the last 30 days (from the current day).
  Showcasing this information as a chart for the last 30 days.
 """
-
-from datetime import timedelta
+from calendar import month
+from datetime import datetime, timedelta
 import pandas as pd #pandas dataframe
 import requests
-import datetime
 
 #api key
-API_KEY = ''
-#base URL for retrieving currency pair from API
-url = 'https://v6.exchangerate-api.com/v6/' + API_KEY + '/pair/'
+API_KEY = 'ddb26c4e1d440ac62ce77fba'
 
-# Base and exchange currencies
+#base URL for retrieving currency rate from API
+url = 'https://v6.exchangerate-api.com/v6/' + API_KEY + '/history/'
+
+# Base and exchange currencies with baseAmount of $1
 baseCurrency = 'USD'
 exchangeCurrency = 'AUD'
+baseAmount = '1.00'
 
-#Gets the current date, day, month, and year
-currentDate = datetime.date.today()
-currentYear = currentDate.year
-currentMonth = currentDate.month
-currentDay = currentDate.day
-
-#
+# Get the current date
+currentDate = datetime.now().date()
 
 # Dictionary to store the last 30 days worth of dates
 datesLastThirtyDays = {}
@@ -38,22 +34,38 @@ for i in range(30):
         'day' : day.day,
         'month': day.month
     }
-    #datesLastThirtyDays.append(day)
 
-for date_str, date_info in datesLastThirtyDays.items():
-    print(f"Date: {date_str}, Year: {date_info['year']}, Month: {date_info['month']}, Day: {date_info['day']}")
+# List to store dates and rates
+exchangeData = []
+
+# Testing to if API call is successful
+#Current Year, Month and Day
+currYear = datesLastThirtyDays[str(currentDate)]['year']
+currMonth = datesLastThirtyDays[str(currentDate)]['month']
+currDay = datesLastThirtyDays[str(currentDate)]['day']
+
+fullURL = f'{url}{baseCurrency}/{currYear}/{currMonth}/{currDay}'
+
+# API request
+response = requests.get(fullURL)
+data = response.json()
+
+print(data)
+
+# Check if request was successful and if we can access certain fields from the request
+if response.status_code == 200 and "conversion_rates" in data:
+    #pull the specific exchange rate, in our case AUD
+    rate = data['conversion_rates'].get(exchangeCurrency)
+    if rate:
+        # If we can get the rate, append the date and the AUD exchange rate to the list
+        exchangeData.append({'date' : currentDate, 'rate' : rate})
+    else:
+        print('No exchange rates available')
+else:
+    print('Failed to retrieve exchange rates')
 
 
-fullURL = url + baseCurrency + '/' + exchangeCurrency + '/' #+ str(amount)
+# Convert the list of dictionaries to a pandas DataFrame
+df = pd.DataFrame(exchangeData)
+print(df)
 
-
-# Making the request
-#response = requests.get(fullURL)
-#data = response.json()
-# print(data)
-
-#dr = pd.DataFrame(
-#    {}
-
-
-#)
